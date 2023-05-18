1006dbc9c8fd5d0aa353552975961e2fbecca1d3
Ariane Garrett <arigabec@gmail.com>
2023-05-18 12:38:22 -0400

    nav correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Footer } from './components/Footer'
-import { Navbar } from './components/NavBar'
+import { Navbar } from './components/Navbar'
 import Hero from './components/Hero'
 import About from "./components/About";
 import Tours from './components/Tours';
